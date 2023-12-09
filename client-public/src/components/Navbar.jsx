3802134eb57e2ef13edabe95972fb2b3a7ce529c
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-12-09 23:48:03 +0700

    final

--- client-public/src/components/Navbar.jsx
+++ client-public/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "assets/logo/png/logo-no-background.png";
+import logo from "../assets/logo/png/logo-no-background.png";
 
 const Navbar = () => {
 	return (
